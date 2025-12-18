e10154ef35ef2d7d90d8d1e7ed52f41e217beea3
bala-amypo <bala@amypo.in>
2025-12-18 16:35:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/Host.java
+++ src/main/java/com/example/demo/entity/Host.java
@@ -10,7 +10,44 @@ public class Host{
     private LocalDateTime createdAt;
 
     public Host(Long id,String hostname,String fullname,String email,String department,String phone,String createdAt){
-        
+        this.id=id;
+        this.hostname=hostname;
+        this.fullname=fullname;
+        this.email=email;
+        this.department=department;
+        this.phone=phone;
+        this.createdAt=createdAt;
+    }
+     public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getHostname(){
+        return hostname;
+    }
+    public void sethostname(String hostname){
+        this.hostname=hostname;
+    }
+    public 
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String Email){
+        this.email=email;
+    }
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String Phone){
+        this.phone=phone;
+    }
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+        }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
     }
 
 }
