e432e218d66eb5dcc24998ee8b7d3cc7dbed0307
bala-amypo <bala@amypo.in>
2025-12-18 16:11:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -6,8 +6,13 @@ public class User{
     private String email;
     private String phone;
     private String idProofNumber;
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
-    public User(Long id,String fullname,String email,)
+    public User(Long id,String fullname,String email,String phone,String idProofNumber,String createdAt){
+        this.id=id;
+        this.fullname=fullname;
+        this.email=email;
+        
+    }
 
 }
