fe0c53cfdfbe997db382c2488e5fa69029149bd9
bala-amypo <bala@amypo.in>
2025-12-18 16:30:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/Host.java
+++ src/main/java/com/example/demo/entity/Host.java
@@ -3,5 +3,10 @@ package com.example.demo.entity;
 public class Host{
     private Long id;
     private String hostName;
-    private String fulln
+    private String fullname;
+    private String email;
+    private String department;
+    private String phone;
+    private LocalDateTime createdAt;
+    
 }
