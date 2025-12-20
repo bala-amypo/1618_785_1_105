5b4c8114fd5609cd76d34ba33f1363e085906231
bala-amypo <bala@amypo.in>
2025-12-20 11:25:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/VisitLogentity.java
+++ src/main/java/com/example/demo/entity/VisitLogentity.java
@@ -1,5 +1,20 @@
 package com.example.demo.entity;
 
+
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class VisitLog{
-    
+@Id
+      private Long id;
+      private visitor;
+      private host;
+      private LocalDateTime checkInTime;
+      private LocalDateTime checkOutTime;
+      private String purpose;
+      private
 }
