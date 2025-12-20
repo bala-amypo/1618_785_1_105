45c7e86f5184c2d5a5df95e67c422baf98ce5b82
bala-amypo <bala@amypo.in>
2025-12-20 11:04:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Appointment.java
+++ src/main/java/com/example/demo/entity/Appointment.java
@@ -1,9 +1,22 @@
 package com.example.demo.entity;
 
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class Appointment{
+    @Id
     private Long id;
     private visitor;
     private host;
-    
+    private LocalDate appointmentDate;
+    private String purpose;
+    private String status;
 }
