40c060b74d5becd5a54e6fb21cf4dc44fc0d4f4d
bala-amypo <bala@amypo.in>
2025-12-20 11:37:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertNotificationentity.java
+++ src/main/java/com/example/demo/entity/AlertNotificationentity.java
@@ -1,9 +1,26 @@
 package com.example.demo.entity;
 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
 
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class AlertNotificationentity{
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long Id;
-    private 
+    private visitLog;
+    private String sentto;
+    private String alertMessage;
+    private LocalDateTime sentAt;
+
 }
