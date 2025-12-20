7584b46f0f6a4a05c4040a5e224354d46a94a238
bala-amypo <bala@amypo.in>
2025-12-20 10:49:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Visitor.java
+++ src/main/java/com/example/demo/entity/Visitor.java
@@ -1,9 +1,11 @@
 package com.example.demo.entity;
 
+@Data
 public class Visitor{
     private Long id;
     private String fullname;
     private String email;
     private String phone;
-    private 
+    private String idProofNumber;
+    private LocalDateTime createdAt;
 }
