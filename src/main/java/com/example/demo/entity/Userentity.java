bb75bfee84a9f9e64bee3604f71f594e3596a7bb
bala-amypo <bala@amypo.in>
2025-12-20 11:40:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/Userentity.java
+++ src/main/java/com/example/demo/entity/Userentity.java
@@ -1,51 +1,29 @@
 package com.example.demo.entity;
 
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    private String fullname;
+    
+    private String username;
+    @Column(name="email" unique='true')
     private String email;
-    private String phone;
-    private String idProofNumber;
+    private String password;
+    private String role;
     private LocalDateTime createdAt;
 
-    public User(Long id,String fullname,String email,String phone,String idProofNumber,String createdAt){
-        this.id=id;
-        this.fullname=fullname;
-        this.email=email;
-        this.phone=phone;
-        this.idProofNumber=idProofNumber;
-        this.createdAt=createdAt;
-    }
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getFullname(){
-        return fullname;
-    }
-    public void setFullname(String fullname){
-        this.fullname=fullname;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(String Email){
-        this.email=email;
-    }
-    public String getPhone(){
-        return phone;
-    }
-    public void setPhone(String Phone){
-        this.phone=phone;
-    }
-    public LocalDateTime getCreatedAt(){
-        return createdAt;
-        }
-    public void setCreatedAt(LocalDateTime createdAt){
-        this.createdAt=createdAt;
-        
-        }
-     }
-    
+}
