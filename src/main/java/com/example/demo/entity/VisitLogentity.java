3d1afbe1432c02bdcd8951503f683f800d187c12
bala-amypo <bala@amypo.in>
2025-12-20 11:31:03 +0000

    User commit

--- src/main/java/com/example/demo/entity/VisitLogentity.java
+++ src/main/java/com/example/demo/entity/VisitLogentity.java
@@ -1,6 +1,13 @@
 package com.example.demo.entity;
 
-
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.time.LocalDateTime;
 
 
 
