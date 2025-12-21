f48d7068f6186f30eddde087195d802ba7ee7760
bala-amypo <bala@amypo.in>
2025-12-21 06:20:55 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitorService.java
+++ src/main/java/com/example/demo/service/VisitorService.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public interface VisitorService {
 
-    // Create a new visitor
+    
     Visitor createVisitor(Visitor visitor);
 
-    // Get visitor by ID (throw exception if not found)
+   
     Visitor getVisitor(Long id);
 
-    // Get all visitors
+    
     List<Visitor> getAllVisitors();
 }
