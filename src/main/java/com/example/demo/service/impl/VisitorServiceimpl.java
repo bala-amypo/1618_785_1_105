63bf56ce110f02ad0cb807ca862492896764a778
bala-amypo <bala@amypo.in>
2025-12-21 07:32:55 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VisitorServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceimpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class VisitorServiceimpl implements VisitorService {
+public class VisitorServiceImpl implements VisitorService {
 
     @Autowired
     private VisitorRepository visitorRepository;
