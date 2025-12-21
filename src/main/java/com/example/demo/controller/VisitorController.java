ebfa3a19552711352f3c805ea5d53768e5f7d298
bala-amypo <bala@amypo.in>
2025-12-21 07:02:55 +0000

    User commit

--- src/main/java/com/example/demo/controller/VisitorController.java
+++ src/main/java/com/example/demo/controller/VisitorController.java
@@ -2,35 +2,36 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Visitor;
 import com.example.demo.service.VisitorService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/visitors")
-@Tag(name = "Visitor Controller", description = "APIs for managing visitors")
 public class VisitorController {
 
     @Autowired
     private VisitorService visitorService;
 
-    // POST /api/visitors - create visitor
-    @PostMapping
+    
+    @PostMapping("/api/visitors")
     public Visitor createVisitor(@RequestBody Visitor visitor) {
         return visitorService.createVisitor(visitor);
     }
 
-    // GET /api/visitors - list visitors
-    @GetMapping
+    
+    @GetMapping("/api/visitors")
     public List<Visitor> getAllVisitors() {
         return visitorService.getAllVisitors();
     }
 
-    // GET /api/visitors/{id} - get visitor by id
-    @GetMapping("/{id}")
+    
+    @GetMapping("/api/visitors/{id}")
     public Visitor getVisitor(@PathVariable Long id) {
         return visitorService.getVisitor(id);
     }
