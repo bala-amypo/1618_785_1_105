f8dbfb59267afda6df091dbe58b7c0ddec9c4125
bala-amypo <bala@amypo.in>
2025-12-26 15:55:50 +0000

    User commit

--- src/main/java/com/example/demo/controller/HostController.java
+++ src/main/java/com/example/demo/controller/HostController.java
@@ -2,30 +2,31 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Host;
 import com.example.demo.service.HostService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/hosts")
 public class HostController {
 
-    @Autowired
-    private HostService hostService;
+    private final HostService hostService;
+
+    public HostController(HostService hostService) {
+        this.hostService = hostService;
+    }
 
     @PostMapping
     public Host create(@RequestBody Host host) {
         return hostService.createHost(host);
     }
 
-    @GetMapping("/{id}")
-    public Host get(@PathVariable Long id) {
-        return hostService.getHost(id);
-    }
-
     @GetMapping
     public List<Host> getAll() {
         return hostService.getAllHosts();
+    }
+
+    @GetMapping("/{id}")
+    public Host get(@PathVariable Long id) {
+        return hostService.getHost(id);
     }
 }
