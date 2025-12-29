e8d6e0e9d9fb31475211bc879aadd67ef973a9b4
bala-amypo <bala@amypo.in>
2025-12-29 04:57:36 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -31,6 +31,7 @@ public class SwaggerConfig {
                 )
 
                
+               
                 .components(
                         new Components()
                                 .addSecuritySchemes(
